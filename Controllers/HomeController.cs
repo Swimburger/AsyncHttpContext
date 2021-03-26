26f2eaa48c327275f4f8cbc5c7a0a910ad6d49a9
Niels Swimberghe <nielsswimberghe@gmail.com>
2021-03-25 23:21:21 -0400

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AsyncHttpContext.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult IncrementWithStaticSession()
+        {
+            MyStaticSessionWrapper.Counter++;
+            return View("Index");
+        }
+
+        public async Task<ActionResult> IncrementWithStaticSessionAsync()
+        {
+            await Task.Run(() =>
+            {
+                MyStaticSessionWrapper.Counter++;
+            });
+            return View("Index");
+        }
+
+        public ActionResult Increment()
+        {
+            var mySession = new MySessionWrapper(Session);
+            mySession.Counter++;
+            return View("Index");
+        }
+
+        public async Task<ActionResult> IncrementAsync()
+        {
+            await Task.Run(() =>
+            {
+                var mySession = new MySessionWrapper(Session);
+                mySession.Counter++;
+            });
+            return View("Index");
+        }
+
+        public ActionResult IncrementWithExtensionMethods()
+        {
+            Session.SetCounter(Session.GetCounter() + 1);
+            return View("Index");
+        }
+
+        public async Task<ActionResult> IncrementWithExtensionMethodsAsync()
+        {
+            await Task.Run(() =>
+            {
+                Session.SetCounter(Session.GetCounter() + 1);
+            });
+            return View("Index");
+        }
+    }
+}
---
+++ MySessionClasses.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.SessionState;
+
+namespace AsyncHttpContext
+{
+    public static class MyStaticSessionWrapper
+    {
+        private const string CounterKey = "MyCounter";
+        private static HttpSessionState Session => HttpContext.Current.Session;
+
+        public static int Counter
+        {
+            get => (int)(Session[CounterKey] ?? 0);
+            set => Session[CounterKey] = value;
+        }
+    }
+
+    public class MySessionWrapper
+    {
+        private const string CounterKey = "MyCounter";
+        private readonly HttpSessionStateBase session;
+
+        public MySessionWrapper(HttpSessionStateBase session)
+        {
+            this.session = session;
+        }
+
+        public int Counter
+        {
+            get => (int)(session[CounterKey] ?? 0);
+            set => session[CounterKey] = value;
+        }
+    }
+
+    public static class MySessionExtensions
+    {
+        private const string CounterKey = "MyCounter";
+
+        public static int GetCounter(this HttpSessionStateBase session)
+            => (int)(session[CounterKey] ?? 0);
+
+        public static void SetCounter(this HttpSessionStateBase session, int count)
+            => session[CounterKey] = count;
+    }
+}
